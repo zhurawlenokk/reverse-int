4264c32fb22b7390bd864f8228138cad49e4ee9a
Zhurawlenokk <zhuravlevamary36@gmail.com>
2021-03-14 22:21:08 +0400

    init: add function

--- src/index.js
+++ src/index.js
@@ -1,3 +1,15 @@
 module.exports = function reverse (n) {
-  
+    n = Math.abs(n);
+       
+    let array = n.toString().split('');
+
+    let num = Number(array.reverse().join(''));
+    
+    return num
+    
 }
+
+
+
+
+
